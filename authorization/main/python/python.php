1184ca7d24be4dbd668326b289e87118d0bb3abd
OnePuchPotato <zhibek.akylbekkyzy@alatoo.edu.kg>
2022-12-17 04:01:49 +0600

    changed the structure of files

--- authorization/main/python/python.php
+++ authorization/main/python/python.php
@@ -19,7 +19,19 @@
         <div class="square">
             <h2>Task 1</h2>
             <h3>Take two lists, say for example these two:<br>
-                <h3 class="first">a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]<br>
+                <h3 class="first"> a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]<br>
+                    <h3 class="second"> b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+                    </h3>
+                    <form action="upload.php" method="post" enctype="multipart/form-data">
+                        <input class="input" type="file" name="file">
+                        <input class="input" type="submit" value="Upload">
+                    </form>
+        </div>
+
+        <div class="square">
+            <h2>Task 2</h2>
+            <h3>Take two lists, say for example these two:<br>
+                <h3 class="first"> a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]<br>
                     <h3 class="second"> b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
                     </h3>
                     <form action="upload.php" method="post" enctype="multipart/form-data">
