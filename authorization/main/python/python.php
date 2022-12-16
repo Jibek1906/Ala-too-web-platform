5557ab3b1e5f9eea21107e8382f89b7fc0d0585a
OnePuchPotato <zhibek.akylbekkyzy@alatoo.edu.kg>
2022-12-17 03:43:06 +0600

    Added task to python course

---
+++ authorization/main/python/python.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Log in</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="python.css" type="text/css" />
+</head>
+
+<body>
+    <header>
+        <h1><img src="../img/python.webp" width="150px" height="150px">Python</h1>
+    </header>
+    <div id="body">
+
+        <div class="square">
+            <h2>Task 1</h2>
+            <h3>Take two lists, say for example these two:<br>
+                <h3 class="first">a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]<br>
+                    <h3 class="second"> b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+                    </h3>
+                    <form action="upload.php" method="post" enctype="multipart/form-data">
+                        <input class="input" type="file" name="file">
+                        <input class="input" type="submit" value="Upload">
+                    </form>
+        </div>
+    </div>
+
+</body>
+
+</html>
