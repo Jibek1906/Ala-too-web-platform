c3d6e914ac66281618a23bf2e241817fdcdb03ed
OnePuchPotato <zhibek.akylbekkyzy@alatoo.edu.kg>
2022-12-18 03:41:30 +0600

    Java course created

--- authorization/main/java/java.php
+++ authorization/main/java/java.php
@@ -1,62 +1,108 @@
-<!-- left bar starts -->
-<div class="left-bar">
-    <div class="upper-part">
-      <div class="actions">
-        <div class="circle"></div>
-        <div class="circle-2"></div>
-      </div>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Log in</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="java.css" type="text/css" />
+</head>
+
+<body>
+    <header>
+        <h1><a href="../../main.html"><img id="back" src="../img/back.png" width="90px" height="90px"></a><img
+                class="img-c" src="../img/java.png" width="150px" height="150px">Java</h1>
+    </header>
+    <div id="body">
+
+        <div class="square" id="square-first">
+            <h2><strong>Task 1</strong></h2>
+            <h3>Java program to check whether the given number is even or odd<br>
+                <h4><strong>Example of Output:</strong></h4>
+                <h3 class="text">
+                    Enter a number:<br>
+                    24<br>
+                    The number is even<br>
+                    -----------------------------------<br>
+                    Enter a number:<br>
+                    33<br>
+                    The number is odd
+                </h3>
+                <h4 class="req"><strong>Requests</strong></h4>
+                <h3 class="req-text">Code should be:<br>
+                    valid<br>
+                    clean<br>
+                    no comments please</h3>
+                <form action="upload.php" method="post" enctype="multipart/form-data">
+                    <input class="input" type="file" name="file">
+                    <input class="input" type="submit" value="Upload">
+                </form>
+        </div>
+
+        <div class="square" id="square-second">
+            <h2><strong>Task 2</strong></h2>
+            <h3>Java program to find out the average of a set of integers<br>
+                <h4><strong>Example of Output:</strong></h4>
+                <h3 class="text">
+                    Enter the count of numbers:<br>
+                    5<br>
+                    Enter an integer:<br>
+                    3<br>
+                    Enter an integer:<br>
+                    8<br>
+                    Enter an integer:<br>
+                    6<br>
+                    Enter an integer:<br>
+                    7<br>
+                    Enter an integer:<br>
+                    4<br>
+                    The average is: 5.6
+                </h3>
+                <h4 class="req"><strong>Requests</strong></h4>
+                <h3 class="req-text">Code should be:<br>
+                    valid<br>
+                    clean<br>
+                    no comments please</h3>
+                <form action="upload.php" method="post" enctype="multipart/form-data">
+                    <input class="input" type="file" name="file">
+                    <input class="input" type="submit" value="Upload">
+                </form>
+        </div>
+
+        <div class="square" id="square-third">
+            <h2><strong>Task 3</strong></h2>
+            <h3>Java program to display all the multiples of 3 within the range 10 to 50<br>
+                <h4><strong>Expected Output:</strong></h4>
+                <h3 class="text">
+                    12<br>
+                    15<br>
+                    18<br>
+                    21<br>
+                    24<br>
+                    27<br>
+                    30<br>
+                    33<br>
+                    36<br>
+                    39<br>
+                    42<br>
+                    45<br>
+                    48<br>
+                </h3>
+                <h4 class="req"><strong>Requests</strong></h4>
+                <h3 class="req-text">Code should be:<br>
+                    valid<br>
+                    clean<br>
+                    no comments please</h3>
+                <form action="upload.php" method="post" enctype="multipart/form-data">
+                    <input class="input" type="file" name="file">
+                    <input class="input" type="submit" value="Upload">
+                </form>
+        </div>
+
     </div>
-  
-    <!-- left content starts -->
-    <div class="left-content">
-      <ul class="action-list">
-        <li class="item">
-          <img class="feather feather-inbox" src="svg/inbox.svg" alt="" />
-          <span>Inbox</span>
-        </li>
-        <li class="item">
-          <img class="feather feather-star" src="svg/star.svg" alt="" />
-          <span> Today</span>
-        </li>
-        <li class="item">
-          <img class="feather feather-calendar" src="svg/calender.svg" alt="" />
-          <span>Upcoming</span>
-        </li>
-        <li class="item">
-          <img class="feather feather-hash" src="svg/hash.svg" alt="" />
-          <span>Important</span>
-        </li>
-        <li class="item">
-          <img class="feather feather-users" src="svg/users.svg" alt="" />
-          <span>Meetings</span>
-        </li>
-        <li class="item">
-          <img class="feather feather-trash" src="svg/trash.svg" alt="" />
-          <span>Trash</span>
-        </li>
-      </ul>
-  
-      <ul class="category-list">
-        <li class="item">
-          <img class="feather feather-users" src="svg/users.svg" alt="" />
-          <span>Family</span>
-        </li>
-        <li class="item">
-          <img class="feather feather-sun" src="svg/sun.svg" alt="" />
-          <span>Vacation</span>
-        </li>
-        <li class="item">
-          <img class="feather feather-trending-up" src="svg/trending.svg" alt="" />
-          <span>Festival</span>
-        </li>
-        <li class="item">
-          <img class="feather feather-zap" src="svg/zap.svg" alt="" />
-          <span>Concerts</span>
-        </li>
-      </ul>
-    </div>
-    <!-- left content ends -->
-  </div>
-  <!-- left bar ends -->
-  
-  
+
+</body>
+
+</html>
