a02f1e60b6a562eecf36f12143724a793ddb7d2e
OnePuchPotato <zhibek.akylbekkyzy@alatoo.edu.kg>
2022-12-18 01:22:46 +0600

    C++ course and change style.css of index.html

--- authorization/main/c++/c++.php
+++ authorization/main/c++/c++.php
@@ -1,55 +1,99 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <title>Tasks</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Log in</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/c++.css">
+    <link rel="stylesheet" href="c.css" type="text/css" />
 </head>
+
 <body>
-   <div class="container">
-      <h1>Tasks C++</h1>
+    <header>
+        <h1><a href="../../main.html"><img id="back" src="../img/back.png" width="90px" height="90px"></a><img class="img-c" src="../img/c++.png" width="150px" height="150px"></h1>
+    </header>
+    <div id="body">
 
-        <section><div class="row">
-           <form class="animated fadeIn" id="form">
-           <div class="">
-            <div class="s12 l12 m12">
+        <div class="square">
+            <h2><strong>Task 1</strong></h2>
+            <h3>Write a program in C++ to print the sum of two numbers.<br>
+                <h4><strong>Sample Output</strong></h4>
+                <h3 class="text">Display arithmetic operations with mixed data type :<br>
+                    -----------------------------------<br>
+                    The sum of 29 and 30 is : 59<br></h3>
+                    <h4 class="req"><strong>Requests</strong></h4>
+                    <h3 class="req-text">Code should be:<br>
+                        valid<br>
+                        clean<br>
+                        no comments please</h3>
+                    <form action="upload.php" method="post" enctype="multipart/form-data">
+                        <input class="input" type="file" name="file">
+                        <input class="input" type="submit" value="Upload">
+                    </form>
+        </div>
 
-                <input type="text" id="name" placeholder="Name">
-                <input type="text" id="subject" placeholder="Subject">
-                <input type="date" id="myDate">
-            </div>
-               
-               <div class="row">
-                <div class="s10 l10 m10">
-                <textarea id="description" placeholder="Requests" cols="30" rows="10" onClick="myObj.textSelect(this)"> Requests</textarea>
-                               
+        <div class="square" id="square-second">
+            <h2><strong>Task 2</strong></h2>
+            <h3>Write a program in C++ to display various type or arithmetic operation using mixed data type.<br>
+                <h4><strong>Sample Output</strong></h4>
+                <h3 class="text">Display arithmetic operations with mixed data type :<br>
+                    -----------------------------------<br>
+                    5 + 7 = 12<br>
+                    3.7 + 8.0 = 11.7<br>
+                    5 + 8.0 = 13.0<br>
+                    5 - 7 = -2<br>
+                    3.7 - 8.0 = -4.3<br>
+                    5 - 8.0 = -3.0<br>
+                    5 * 7 = 35<br>
+                    3.7 * 8.0 = 29.6<br>
+                    5 * 8.0 = 40.0<br>
+                    5 / 7 = 0<br>
+                    3.7 / 8.0 = 0.5<br>
+                    5 / 8.0 = 0.6
 
-            </div>
-            </div>
-            <div class="col s12 l12 m12">
-                <div class="btn" onclick="submitInfo()">Submit</div>
-                <div class="btn" onclick="myObj.hide()">Close</div>
-            </div>
+                    <h4 class="req"><strong>Requests</strong></h4>
+                    <h3 class="req-text">Code should be:<br>
+                        valid<br>
+                        clean<br>
+                        no comments please</h3>
+                    </h3>
+                    <form action="upload.php" method="post" enctype="multipart/form-data">
+                        <input class="input" type="file" name="file">
+                        <input class="input" type="submit" value="Upload">
+                    </form>
+        </div>
 
-            </div>
-            </form>
-        </div></section>
-       <div class="btn" onClick="myObj.show()" id="show">New Task</div>
-<!--       TASK LIST-->
-  
-          <div class="row" style="text-align:center;" id="myTasks">
-             
+        <div class="square" id="square-third">
+            <h2><strong>Task 3</strong></h2>
+            <h3> Write a program in C++ to display the operation of pre and post increment and decrement.<br>
+                <h4><strong>Sample Output</strong></h4>
+                <h3 class="text">Display the operation of pre and post increment and decrement :<br>
+                    -----------------------------------<br>
+                    
+                    The number is : 57<br>
+                    After post increment by 1 the number is : 58<br>
+                    After pre increment by 1 the number is : 59<br>
+                    After increasing by 1 the number is : 60<br>
+                    After post decrement by 1 the number is : 59<br>
+                    After pre decrement by 1 the number is : 58<br>
+                    After decreasing by 1 the number is : 57
 
-              
-          </div>    
-   </div>
-   <script src="c++.js"></script>
+                    <h4 class="req"><strong>Requests</strong></h4>
+                    <h3 class="req-text">Code should be:<br>
+                        valid<br>
+                        clean<br>
+                        no comments please</h3>
+                    </h3>
+                    <form action="upload.php" method="post" enctype="multipart/form-data">
+                        <input class="input" type="file" name="file">
+                        <input class="input" type="submit" value="Upload">
+                    </form>
+        </div>
 
+    </div>
 
+</body>
 
-    </body>
 </html>
